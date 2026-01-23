32532f9175413301bed9ccd5b4b552ca26e1108f
Glaucoft <eng_gft@yahoo.com.br>
2026-01-23 02:05:34 +0000

    01 - Docker

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -3,7 +3,7 @@
 
 # Este script foi convertido de um Jupyter Notebook através do comando:
 # uv run jupyter nbconvert --to=script notebook.ipynb
-
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -48,21 +48,32 @@ parse_dates = [
 
 #print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
+#def run():
 
-def run():
+#    pg_user = 'root'
+#    pg_pass = 'root'
+#    pg_host = 'localhost'
+#    pg_port = 5432
+#    pg_db = 'ny_taxi'
 
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
+#    year = 2021
+#    month = 1
 
-    year = 2021
-    month = 1
+#    target_table = 'yellow_taxi_data'
 
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 1000000
+#    chunksize = 1000000
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
