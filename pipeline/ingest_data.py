2d4795e45ecc09d1ff427cee3f964c68db37eec2
Glaucoft <eng_gft@yahoo.com.br>
2026-01-24 06:40:11 +0000

    Homework

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# HOMEWORK: Script copiado do original e adequado para ler Parquet
+
 # Este script foi convertido de um Jupyter Notebook através do comando:
 # uv run jupyter nbconvert --to=script notebook.ipynb
 import click
 import pandas as pd
+import pyarrow.parquet as pq
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
-#df = pd.read_csv(url)
-#df.head()
-#len(df)
-#df['VendorID']
 
 dtype = {
     "VendorID": "Int64",
@@ -37,85 +35,60 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-#df = pd.read_csv(
-#    url,
-#    nrows=100,
-#    dtype=dtype,
-#    parse_dates=parse_dates
-#)
-
-#get_ipython().system('uv add sqlalchemy psycopg2-binary')
-
-#print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
 @click.command()
 @click.option('--pg-user', default='root', help='PostgreSQL user')
 @click.option('--pg-pass', default='root', help='PostgreSQL password')
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+
+#@click.option('--year', default=2021, type=int, help='Year of the data')
+#@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--year', default=2025, type=int, help='Year of the data')
+@click.option('--month', default=11, type=int, help='Month of the data')
+
+@click.option('--target-table', default='yellow_taxi_trips_2025_11', help='Target table name')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
-def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+#def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+
+@click.option('--input-file', default='/data/yellow_tripdata_2025-11.parquet', help='Caminho do arquivo Parquet dentro do container')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize, input_file):
+
     """Ingest NYC taxi data into PostgreSQL database."""
-#def run():
 
-#    pg_user = 'root'
-#    pg_pass = 'root'
-#    pg_host = 'localhost'
-#    pg_port = 5432
-#    pg_db = 'ny_taxi'
+ # INICIO -- Direto do site oficial não foi possível, por isso o uso do arquivo localmente
+ #    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+ #    url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
+    prefix = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
+    url = f'{prefix}/yellow_tripdata_2025-11.parquet'
+    
+ #    url = input_file
+ # FIM
 
-#    year = 2021
-#    month = 1
-
-#    target_table = 'yellow_taxi_data'
-
-#    chunksize = 1000000
-
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-
+    print(f"Lendo Parquet local: {input_file}")
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
-    )
+    print("Lendo arquivo Parquet em batches...")
+    table = pq.read_table(input_file)
 
-    for df_chunk in df_iter:
-        print(len(df_chunk))
+    print(f"Total de linhas no Parquet: {table.num_rows}")
 
-    #get_ipython().system('uv add tqdm')
-
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=100000
-    )
-
+    print("Gravando no PostgreSQL...")
     first = True
-    for df_chunk in tqdm(df_iter):
-        if first:
-            df_chunk.head(n=0).to_sql(
-                name=target_table, 
-                con=engine, 
-                if_exists='replace'
-            )
-        first = False
-        df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
-            if_exists='append'
+    for batch in table.to_batches(max_chunksize=chunksize):
+        df = batch.to_pandas()
+
+        df.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='replace' if first else 'append',
+            index=False
         )
 
+        print(f"Inseridas {len(df)} linhas")
+        first = False
+
+    print("Ingest concluído com sucesso 🚀")
 
 if __name__ == '__main__':
     run()
